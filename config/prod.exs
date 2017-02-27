16dba018cb78c104dc9fbe56e52aa24708102141
Oliver Bøving <oliverboving@gmail.com>
2017-02-27 14:22:01 +0100

    fix spelling error in config

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :chess, Chess.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "mysterious-caverns-81656.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forward_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
